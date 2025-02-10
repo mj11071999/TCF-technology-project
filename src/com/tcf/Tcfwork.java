b836c8fbc5e9367dc21ab685d4d46256a6ea4548
mj11071999 <jervice123@gmail.com>
2025-02-10 09:41:26 +0530

    Adding tcf project coverage

---
+++ src/com/tcf/Tcfwork.java
@@ -0,0 +1,14 @@
+package com.tcf;
+
+public class Tcfwork {
+public static void main(String[] args) {
+	int age =78;
+	if (age>=45) {
+		System.out.println("old age");
+	} else {
+		System.out.println("not old age");
+
+	}
+}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TCF_Technology {
+}
